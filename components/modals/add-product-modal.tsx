b17584d49fb5df3857eb65d2bb277eaaf8d9ea6c
kelpejol <paulkayode23@gmail.com>
2025-08-11 14:11:52 +0100

    did

--- components/modals/add-product-modal.tsx
+++ components/modals/add-product-modal.tsx
@@ -40,7 +40,19 @@ export const PRODUCT_CATEGORIES = [
   { value: "footwears", label: "Footwears" },
   { value: "bags", label: "Bags" },
   { value: "accessories", label: "Accessories" },
-  {value: "curated_fashion", label: "Curated Fashion"}
+  { value: "curated_fashion", label: "Curated Fashion" },
+  { value: "home_decor", label: "Home Decor" },
+  { value: "beauty&_fragrance", label: "Beauty & Fragrance" },
+  { value: "fitness&_sports", label: "Fitness & Sports" },
+
+  { value: "books", label: "Books" },
+  { value: "toys&_games", label: "Toys & Games" },
+
+  { value: "pet_supplies", label: "Pet Supplies" },
+  { value: "art&_crafts", label: "Art & Crafts" },
+
+  { value: "baby_products", label: "Baby Products" },
+
 ];
 
 interface Variation {
--- components/modals/edit-product-modal.tsx
+++ components/modals/edit-product-modal.tsx
@@ -747,14 +747,6 @@
 //   )
 // }
 
-
-
-
-
-
-
-
-
 "use client";
 import type React from "react";
 import { useState, useRef, useEffect } from "react";
@@ -796,6 +788,17 @@ export const PRODUCT_CATEGORIES = [
   { value: "bags", label: "Bags" },
   { value: "accessories", label: "Accessories" },
   { value: "curated_fashion", label: "Curated Fashion" },
+  { value: "home_decor", label: "Home Decor" },
+  { value: "beauty&_fragrance", label: "Beauty & Fragrance" },
+  { value: "fitness&_sports", label: "Fitness & Sports" },
+
+  { value: "books", label: "Books" },
+  { value: "toys&_games", label: "Toys & Games" },
+
+  { value: "pet_supplies", label: "Pet Supplies" },
+  { value: "art&_crafts", label: "Art & Crafts" },
+
+  { value: "baby_products", label: "Baby Products" },
 ];
 
 interface Variation {
